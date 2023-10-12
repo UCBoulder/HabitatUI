6571e3d8a12e69276beb8e90955509596bec4715
Ethan Way <ethan.way@comcast.net>
2023-10-12 11:24:02 -0600

    Made a button in the button center of the map

--- App.tsx
+++ App.tsx
@@ -1,23 +1,23 @@
-import React, {useEffect} from "react";
-import {Alert, StyleSheet, Text, View} from "react-native";
+import React, { useEffect } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
-import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
-import {request, PERMISSIONS} from "react-native-permissions"
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { request, PERMISSIONS } from "react-native-permissions";
 
 export default function App() {
   useEffect(() => {
     // Request location permissions
     request(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION)
-    .then((result) => {
-      if (result === "granted") {
-        console.log("Location permission granted");
-      } else {
-        console.log("Location permission denied");
-      }
-    })
-    .catch((error) => {
-      console.error("Error requesting location permission: ", error);
-    });
+      .then((result) => {
+        if (result === "granted") {
+          console.log("Location permission granted");
+        } else {
+          console.log("Location permission denied");
+        }
+      })
+      .catch((error) => {
+        console.error("Error requesting location permission: ", error);
+      });
   }, []);
 
   return (
@@ -27,38 +27,36 @@ export default function App() {
         <Text style={styles.bannerText}>Example Map</Text>
       </View>
 
-      <View style={styles.locationButtonStyle}>
-      <Button //press the button that gets the user's location
-        title="Get user location"
-        onPress={() => Alert.alert('Pressed')}
+      <View style={styles.mapContainer}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          // Specify coordinates for Gunnison, Colorado
+          initialRegion={{
+            latitude: 38.5449,
+            longitude: -106.9329,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
         />
-      </View>
 
-      {/* Render the map */}
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        // Specify coordinates for Gunnison, Colorado
-        initialRegion={{
-          latitude: 38.5449,
-          longitude: -106.9329,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-      />
+        {/* Location button */}
+        <View style={styles.locationButton}>
+          <Button
+            title="Get user location"
+            onPress={() => Alert.alert('Pressed')} //change this to get cords
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-  },
-  map: {
-    flex: 1, 
-    width: "100%",
+    flex: 1,
   },
   banner: {
     backgroundColor: "blue",
@@ -67,14 +65,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   bannerText: {
-    color: "white", 
+    color: "white",
     fontSize: 18,
   },
-  locationButtonStyle: {
-    justifyContent: 'center',
-    position: 'absolute',
-    bottom: 35,
-    margin: 16,
-    right: 10,
+  mapContainer: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  locationButton: {
+    position: 'relative',
+    height: 1000, 
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
