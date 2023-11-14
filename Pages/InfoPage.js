80c5622b3a5ab86bc25c90ad1e7a1718a0eb5317
Ethan Way <ethan.way@comcast.net>
2023-11-13 19:29:22 -0700

    adding pictures from petar

--- Pages/InfoPage.js
+++ Pages/InfoPage.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 
-const InfoPage = ({ navigation }) => {
+const InfoPage = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Add information from Petar</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+
+      <Image source={require('../images/CheatgrassInfographic.png')} style={styles.infographic} />
+      {/* <Image source={require('../images/CheatGrassBrochure-1.png')} style={styles.image} /> */}
+
+      
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  infographic: {
+    width: 250,
+    height: 625, 
+  },
+  image: {
+    width: 550,
+    height: 250,
+  }
+});
 
 export default InfoPage;
