71dbc2fe20a9fd538ff6e18809a28564a8bed440
Ethan Way <ethan.way@comcast.net>
2023-12-01 13:38:47 -0700

    split files for permissions and observation

---
+++ utils/MakeObservation.js
@@ -0,0 +1,11 @@
+import Geolocation from "react-native-geolocation-service";
+import { sendLocationPin } from "./APICalls";
+
+// helper to combine button functionality and sending to the API
+export const makeObservation = (setUserLocation) => {
+    Geolocation.getCurrentPosition((position) => {
+      setUserLocation(position);
+      sendLocationPin(position);
+    });
+  };
+  
