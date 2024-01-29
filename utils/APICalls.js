6646d3e49a5b63dbbff927b257be5ef3f5eedf78
Ethan Way <ethan.way@comcast.net>
2024-01-29 14:18:20 -0700

    utils

--- utils/APICalls.js
+++ utils/APICalls.js
@@ -1,67 +1,63 @@
-import axios from "axios";
-import RNFS from 'react-native-fs';
-import { Alert } from 'react-native';
+import axios from 'axios'
+import RNFS from 'react-native-fs'
+import { Alert } from 'react-native'
 
 // recieve one or many lat long coordinates from the API
 export const getLocationPins = async () => {
   try {
-    const locTestResponse = await axios.get("http://192.168.56.1:3000/locTest");
-    return locTestResponse.data;
+    const locTestResponse = await axios.get('http://192.168.56.1:3000/locTest')
+    return locTestResponse.data
   } catch (error) {
-    console.error("Error fetching API data: ", error);
-    return null;
+    console.error('Error fetching API data: ', error)
+    return null
   }
-};
+}
 
 // send one lat long coordinate to the API
 export const sendLocationPin = async (position, userID, text, imageSource) => {
-
   const observation = {
-    userID: userID,
+    userID,
     coords: {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
-      accuracy: position.coords.accuracy,
+      accuracy: position.coords.accuracy
     },
     Notes: text,
     VerificationRating: 1,
-    timestamp: position.timestamp,
-  };
+    timestamp: position.timestamp
+  }
 
   try {
-
     if (imageSource) {
-      const imageBase64 = await RNFS.readFile(imageSource, 'base64');
+      const imageBase64 = await RNFS.readFile(imageSource, 'base64')
 
-      const formData = new FormData();
+      const formData = new FormData()
       formData.append('image', {
         uri: imageSource,
         type: 'image/jpeg',
         name: 'image.jpg',
-        encoded64: imageBase64,
-      });
+        encoded64: imageBase64
+      })
 
-      observation.image = imageBase64;
-
+      observation.image = imageBase64
     }
     // console.log(observation.image._parts)
-    const response = await axios.post("http://192.168.56.1:3000/observations",
-      observation,
-    );
-    console.log("Response from backend: ", response.data);
-    Alert.alert("Success", "Your Observation Was Successfully Uploaded")
-
+    const response = await axios.post('http://192.168.56.1:3000/observations',
+      observation
+    )
+    console.log('Response from backend: ', response.data)
+    Alert.alert('Success', 'Your Observation Was Successfully Uploaded')
   } catch (error) {
-    console.error("Error sending data to backend: ", error);
+    console.error('Error sending data to backend: ', error)
 
-    Alert.alert("Failed", "Your Observation Failed to Upload", [{
+    Alert.alert('Failed', 'Your Observation Failed to Upload', [{
       text: 'Retry',
-      onPress: () => sendLocationPin(position, userID, text, imageSource),
+      onPress: () => sendLocationPin(position, userID, text, imageSource)
     },
     {
-      text: 'Ok',
-    }]);
+      text: 'Ok'
+    }])
 
     return null
   }
-};
+}
--- utils/Permissions.js
+++ utils/Permissions.js
@@ -1,29 +1,28 @@
-import { request, PERMISSIONS } from "react-native-permissions";
+import { request, PERMISSIONS } from 'react-native-permissions'
 
 // request location permissions
 export const requestLocationPermission = async () => {
   try {
-    const result = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-    if (result === "granted") {
-      console.log("Location permission granted");
+    const result = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+    if (result === 'granted') {
+      console.log('Location permission granted')
     } else {
-      console.log("Location permission denied");
+      console.log('Location permission denied')
     }
   } catch (error) {
-    console.error("Error requesting location permission: ", error);
+    console.error('Error requesting location permission: ', error)
   }
-};
+}
 
 export const requestCameraPermission = async () => {
   try {
-    const result = await request(PERMISSIONS.ANDROID.CAMERA);
-    if (result === "granted") {
-      console.log("Camera permission granted");
+    const result = await request(PERMISSIONS.ANDROID.CAMERA)
+    if (result === 'granted') {
+      console.log('Camera permission granted')
     } else {
-      console.log("Camera permission denied");
+      console.log('Camera permission denied')
     }
   } catch (error) {
-    console.error("Error requesting camera permission: ", error);
+    console.error('Error requesting camera permission: ', error)
   }
-};
-
+}
