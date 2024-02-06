14d17174fe3f1286cf52b2eeef6a3646f152e61b
Ethan Way <ethan.way@comcast.net>
2024-02-05 17:40:12 -0700

    wild
    This reverts commit 21f86bce6eabfb556d6e4209af8b03b35f3d7913.

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-  emulatorAddress: 'http://10.0.2.2:3000' // Default address for Android emulator
-}
-
-export default config
+    emulatorAddress: 'http://10.0.2.2:3000', // Default address for Android emulator
+  };
+  
+  export default config;
+  
