1afab2ac4c86014f1cfb7054d31bc076a90f5f30
SamBernard2024 <144282601+SamBernard2024@users.noreply.github.com>
2024-03-20 12:51:54 -0600

    Added all the text for the info page, no images yet

--- Pages/InfoPage.js
+++ Pages/InfoPage.js
@@ -17,13 +17,57 @@ const InfoPage = () => {
       <BurgerMenuButton navigation={navigation}/>
     <ScrollView contentContainerStyle={styles.container} >
 
-      <Image source={require('../images/CheatgrassInfographic.png')} style={styles.infographic} />
+      {/* <Image source={require('../images/CheatgrassInfographic.png')} style={styles.infographic} /> */}
       {/* <Image source={require('../images/CheatGrassBrochure-1.png')} style={styles.image} /> */}
 
-      <Text style={styles.header}>This is a header</Text>
+      <Text style={styles.header}>{'\n'}{'\n'}What is Cheatgrass?</Text>
       <Text style={styles.text}>Cheatgrass, an invasive species in Gunnison County, threatens the way of life of the Western
       slope. Whether it is decreasing grass availability for ranching, degrading recreational opportunities, or posing fire threats to homeowners, nearly 
       everybody is affected by Cheatgrass. Through education, outreach, and volunteering we can prevent the spread of Cheatgrass.  </Text>
+
+      <Text style={styles.header}>How do you identify Cheatgrass? </Text>
+      <Text style={styles.text}>-Short flat blade-length (6”-24”) {'\n'}-Sharp seedlet emerging from a “sheath”
+      {'\n'}-Sharp spiky seeds typically containing 5-8 seeds in each seedlet {'\n'}-High density of seeds and stalks in a growing area 
+      {'\n'}-Variable coloration ranging from green to tan {'\n'}-Initially bright green in early life stages 
+      {'\n'}-Turns purplish when it begins to die and then becomes tan or light brown when it dies completely </Text>
+
+      <Text style={styles.header}>Similar speces to look out for </Text>
+
+      <Text style={styles.text}>Indian Rice Grass {'\n'}-Blonde or light green color{'\n'}-Deep roots so it will not come out of the ground 
+      {'\n'}-Spikelets contain a single floret {'\n'}-Loose branches with seeds spread at wide angles </Text>
+
+      <Text style={styles.text}>Needle-and-Thread Grass{'\n'}-Blonde color in the Summer, otherwise greenish
+      {'\n'}-Up to three feet in height {'\n'}-Bunchgrass with deep roots {'\n'}Very long bristles that stick to clothing </Text>
+
+      <Text style={styles.text}>Foxtail Barley{'\n'}-1-2 feet tall{'\n'}-Bristly seeds up to four inches long that resemble the tail of a fox 
+      {'\n'}-About three spikes per node {'\n'}-Seeds can be reddish-brown to purplish in color {'\n'}-Densely packed seeds </Text>
+
+      <Text style={styles.header}>Cheatgrass and Fire: A Deadly Combination </Text>
+
+      <Text style={styles.text}>-Cheatgrass begins to invade the areas between Sagebrush and native grasses, potentially leading to a Cheatgrass monoculture, 
+      which creates a continuous layer of dry fuel{'\n'}-Cheatgrass creates a more fire susceptible landscape, 
+      making it easy to ignite and kill off native sagebrush and fauna{'\n'}-After fires occur, Cheatgrass replaces the Sagebrush, and the cycle continues
+      {'\n'}-Fire become much more frequent, threatening homes and eliminating the Sagebrush and making Cheatgrass the dominant landscape </Text>
+
+      <Text style={styles.header}>Cheatgrass: Bad for Wildlife, Bad for Business  </Text>
+
+      <Text style={styles.text}>-In Nevada, Cheatgrass has replaced native plants and now makes up 90% of the total biomass
+      {'\n'}-The poor forage is bad for wildlife including elk, Sage-Grouse, and deer{'\n'}-Also creates poor grazing conditions for cattle 
+      and decreases the economic value of land{'\n'}-Cheatgrass threatens the largest remaining population of the Gunnison Sage-Grouse, 
+      which will become extinct if the spread of Cheatgrass continues{'\n'}-Many ranching, hunting and other economic and outdoor opportunities will be lost too </Text>
+
+      <Text style={styles.header}>Statistics </Text>
+
+      <Text style={styles.text}>-87% of Gunnison Sage-Grouse population in the Gunnison Valley is threatened
+      {'\n'}-10,000 Cheatgrass plants can grow in one square meter, dramatically increasing the fire risk 
+      {'\n'}-12 million acres have been transformed into Cheatgrass monoculture in the Great Basin </Text>
+
+      <Text style={styles.header}>Don’t Get Cheated </Text>
+
+      <Text style={styles.text}>-Removing Cheatgrass from your property is an effective solution 
+      {'\n'}-Pulling it by hand or using commercially available herbicides are two common forms of removal
+      {'\n'}-Removal is most effective when accompanied by planting of native grasses
+      {'\n'}-Clean boots, gear, and vehicles before traveling to another location to avoid spreading Cheatgrass </Text>
 
     </ScrollView>
     </View>
@@ -34,10 +78,11 @@ const InfoPage = () => {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    justifyContent: 'center'
+    flexGrow: 1,
+    justifyContent: 'space-between'
   },
   infographic: {
-    //width: 350,
+    width: windowWidth,
     objectFit: 'contain',
     resizeMode: 'contain'
     //height: 650
